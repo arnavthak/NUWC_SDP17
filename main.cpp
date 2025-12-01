8a43ecafef4a7133259117d3ff901cd25da091a4
Perchenok, Eli <eli.perchenok@uconn.edu>
2025-12-01 15:13:36 -0500

    STILL WIP: added parsing, CRC and further translation
    NOTE: This code may not be functioning, hence why it's in a new branch. More or less, this is all currently a proof of concept of further development.
    
    What I've worked on here: adding file parsing, CRC values for error checking, and further translation from Python project to C++.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <QCoreApplication>
+#include <QSerialPort>
+#include <QString>
+#include <QDebug>
+#include "serialcomms.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    a.setApplicationName("Qt link test");
+
+    /* Removed for testing/debug purposes. Upon return, please adjust for specific argument quantities.
+    const QStringList args = QCoreApplication::arguments();
+    if (args.size() < 2) {
+        qWarning() << "Error: You must provide the serial port name as argument.";
+        return -1;
+    }*/
+
+    QString sender = "/dev/ttys004";    // please adjust names for arguments in the future.. these ones are placeholders.
+    QString recv = "/dev/ttys005";      // ^^^
+
+    SerialComms comms;
+    comms.linkTest(sender,recv);
+
+    return a.exec();
+}
---
+++ serialcomms.h
@@ -0,0 +1,28 @@
+#ifndef SERIALCOMMS_H
+#define SERIALCOMMS_H
+
+#include <QObject>
+#include <QSerialPort>
+#include <QByteArray>
+
+class SerialComms: public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit SerialComms(QObject *parent = nullptr);
+    void linkTest(const QString &senderPortName, const QString &recvPortName);
+    ~SerialComms();
+
+private slots:
+    void onReceiverReadyRead();
+
+
+private:
+    QSerialPort *m_sendPort;
+    QSerialPort *m_recvPort;
+    QByteArray m_dataSent;
+    QByteArray m_dataReceived;
+};
+
+#endif // SERIALCOMMS_H
